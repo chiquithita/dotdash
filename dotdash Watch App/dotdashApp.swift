02ac63fbbe50886adf3b9bc2296aa9d0ea12ab04
Chiquithita <elizabethchiquithita@gmail.com>
2024-05-20 08:43:02 +0700

    Initial Commit

---
+++ dotdash Watch App/dotdashApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotdashApp.swift
+//  dotdash Watch App
+//
+//  Created by Elizabeth Chiquithita Bunga Ariona Chriska on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct dotdash_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
